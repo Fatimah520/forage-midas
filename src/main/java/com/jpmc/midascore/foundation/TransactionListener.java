d92be05825fabe813fa0cf9be63684d8bd73f36c
Fatimah520 <51966410+Fatimah520@users.noreply.github.com>
2024-12-30 23:23:09 -0800

    finish not yet

--- src/main/java/com/jpmc/midascore/foundation/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/foundation/TransactionListener.java
@@ -1,25 +1,31 @@
 package com.jpmc.midascore.foundation;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TransactionListener {
 
+
+    @PostConstruct
+    public void init(){
+        System.out.println("TransactionListener initialized");
+    }
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    // Kafka Listener Annotation  to listen to messages, read the name of the configuration
-    @KafkaListener(topics = "my-topic", groupId = "midas-core-group")
-
-    // listen method to receive transactions
+    @KafkaListener(topics = "midas-transactions", groupId = "midas-core-group")
     public void listen(String message) {
         try{
             Transaction transaction = objectMapper.readValue(message, Transaction.class);
             System.out.println("Received transaction: " + transaction);
+            System.out.println("Parsed transaction: " + transaction);
         } catch (Exception e){
+            System.err.println("Error processing transaction: " + message);
             e.printStackTrace();
         }
     }
+
 
 }
